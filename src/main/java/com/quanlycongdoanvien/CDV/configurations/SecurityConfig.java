54ab74fe5e8ca52ce3de5caf4cfc15d16f13ed93
tienbaovuong <tienbaoams@gmail.com>
2022-06-14 00:10:03 +0700

    swagger

---
+++ src/main/java/com/quanlycongdoanvien/CDV/configurations/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.quanlycongdoanvien.CDV.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig{
+    @Bean
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
--- src/main/java/com/quanlycongdoanvien/CDV/configurations/SpringFoxConfig.java
+++ src/main/java/com/quanlycongdoanvien/CDV/configurations/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package com.quanlycongdoanvien.CDV.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,5 +17,12 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
