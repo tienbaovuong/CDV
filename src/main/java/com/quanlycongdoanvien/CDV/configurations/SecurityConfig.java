caa156080699dbd262ea2528d1d453c670dce8c8
tienbaovuong <tienbaoams@gmail.com>
2022-06-14 00:14:42 +0700

    swagger

--- src/main/java/com/quanlycongdoanvien/CDV/configurations/SecurityConfig.java
+++ src/main/java/com/quanlycongdoanvien/CDV/configurations/SecurityConfig.java
@@ -3,16 +3,18 @@ package com.quanlycongdoanvien.CDV.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig{
     @Bean
-    protected void configure(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+        return http.build();
     }
 }
