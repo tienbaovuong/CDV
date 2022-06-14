e77764b86dd36c13665f4d131e3fe2e8f0eb1449
tienbaovuong <tienbaoams@gmail.com>
2022-06-14 10:07:54 +0700

    all controllers

--- src/main/java/com/quanlycongdoanvien/CDV/infrastructure/models/PhiThuTruong.java
+++ src/main/java/com/quanlycongdoanvien/CDV/infrastructure/models/PhiThuTruong.java
@@ -1,5 +1,6 @@
 package com.quanlycongdoanvien.CDV.infrastructure.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -24,6 +25,7 @@ import javax.persistence.Table;
         parameters = {@Parameter(name = "sequence_name", value = "SEQ_PHI_THU_TRUONG")})
 @ToString(exclude = {"truong"})
 public class PhiThuTruong extends PhiThu {
+    @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "ID_Truong")
     private Truong truong;
