54ac39d6828d5515fd3746ad94c35d1d66af7821
tienbaovuong <tienbaoams@gmail.com>
2022-07-26 08:51:20 +0700

    Api cap nhat phi thu hang thang

--- src/main/java/com/quanlycongdoanvien/CDV/infrastructure/DTO/PhiThuVienDTO.java
+++ src/main/java/com/quanlycongdoanvien/CDV/infrastructure/DTO/PhiThuVienDTO.java
@@ -6,10 +6,12 @@ public class PhiThuVienDTO {
     public PhiThuVien phiThuVien;
     public String name;
     public Long soCDV;
+    public Long id;
 
     public PhiThuVienDTO(PhiThuVien phiThuVien) {
         this.phiThuVien = phiThuVien;
         this.name = phiThuVien.getVien().getTenVien();
         this.soCDV = 0L;
+        this.id = phiThuVien.getVien().getId();
     }
 }
